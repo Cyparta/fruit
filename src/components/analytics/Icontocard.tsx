1ce3765fb6a0d5b231dd2eb19150d55e15041b0d
Reem99-sina <reemsina2@gmail.com>
2023-05-31 10:37:47 +0300

    fruit main

--- src/components/analytics/Icontocard.tsx
+++ src/components/analytics/Icontocard.tsx
@@ -8,7 +8,7 @@ import imagewallet from "../../../public/assets/image/Group.svg";
 import iconmenu from "../../../public/assets/image/Frame.svg";
 import iconfluent from "../../../public/assets/image/fluent-mdl2_coupon.svg";
 import iconheroicons from "../../../public/assets/image/heroicons-outline_receipt-tax.svg";
-import iconmaterial from "../../../public/assets/image/Vector.svg";
+import iconmaterial from "../../../public/assets/image/Vectorcar.svg";
 import iconmakar from "../../../public/assets/image/akar-icons_shipping-box-02.svg";
 // import iconmenu from "../../../public/assets/image/Frame.svg";
 import Image from "next/image";
